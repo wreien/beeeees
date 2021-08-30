7777e06018d653ca3502ed35ec7b69b16a9af059
Nathaniel Shead <natattak@gmail.com>
2021-08-30 23:16:49 +1000

    Improve website animations.

--- website/main.js
+++ website/main.js
@@ -9,13 +9,141 @@ const websocket = new WebSocket('ws://' + window.location.host + '/observe');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-var world = null;
-var tile_size;
+const world = {
+  tile_size: null,
+  width: null,
+  height: null,
+  map: null,
+  at: function (x, y) { return this.map[x + y * this.width] },
+};
+
+const players = {
+  max: 0,
+  data: new Map(),
+};
+
+const bees = new Map();    // ID -> bee data
+const flowers = new Map(); // ID -> flower data
+
+let last_tick = null;
+let tick_length = null;
+let ticks_per_update = null;
 
 function resize() {
   const th = canvas.height / world.height;
   const tw = canvas.width / world.width;
-  tile_size = Math.min(th, tw);
+  world.tile_size = Math.min(th, tw);
+}
+
+function init(new_world) {
+  Object.assign(world, new_world);
+  resize();
+
+  players.max = world.map.filter(t => t === 'SpawnPoint').length;
+  players.data.clear();
+
+  tick_length = 20;  // 50Hz
+  ticks_per_update = 2000 / tick_length;
+}
+
+function make_ellipse(a, b, initial_t) {
+  return {
+    t: initial_t,
+    x: 0.0,
+    y: 0.0,
+    update: function (new_t) {
+      this.t = new_t;
+      this.x = a * Math.cos(new_t);
+      this.y = b * Math.sin(new_t);
+    },
+  };
+}
+
+function update(data) {
+  for (const h of data.hives) {
+    if (!players.data.has(h.player)) {
+      const colour = 360 * players.data.size / players.max;
+      players.data.set(h.player, Object.assign({ colour, opacity: 0 }, h));
+    }
+  }
+
+  flowers.forEach(v => v.next = null);
+  for (const f of data.flowers) {
+    if (!flowers.has(f.id)) {
+      flowers.set(f.id, { curr: Object.assign({ opacity: 0 }, f), next: f });
+    } else {
+      flowers.get(f.id).next = f;
+    }
+  }
+
+  bees.forEach(v => v.next = null);
+  for (const b of data.bees) {
+    if (!bees.has(b.id)) {
+      const radius = (0.1 + 0.05 * Math.random()) * world.tile_size;
+      const jitter = make_ellipse(
+        Math.random() * 0.35 * world.tile_size,
+        Math.random() * 0.35 * world.tile_size,
+        Math.random() * 2 * Math.PI,
+      );
+      const colour = players.data.get(b.player).colour;
+      bees.set(b.id, { curr: Object.assign({ radius, jitter, colour }, b), next: b });
+    } else {
+      bees.get(b.id).next = b;
+    }
+  }
+}
+
+function animate(num_ticks) {
+  const update_step = num_ticks / ticks_per_update;
+  const time_step = num_ticks * tick_length / 1000;
+
+  players.data.forEach(p => {
+    if (p.opacity < 1.0) {
+      p.opacity = Math.min(1.0, p.opacity + update_step);
+    }
+  });
+
+  flowers.forEach((f, k, m) => {
+    if (f.next === null) {
+      if (f.curr.opacity > 0.0) {
+        f.curr.opacity -= update_step;
+      } else {
+        m.delete(k);
+      }
+    } else {
+      if (f.curr.opacity < 1.0) {
+        f.curr.opacity = Math.min(0.7, f.curr.opacity + update_step);
+      }
+    }
+  });
+
+  bees.forEach((b, k, m) => {
+    if (b.next === null) {
+      m.delete(k);
+    } else {
+      const average_rps = 0.5 * Math.PI * time_step;
+      const t_step = (Math.random() + 0.5) * average_rps;
+      let next_t = b.curr.jitter.t + t_step;
+      if (next_t > 2 * Math.PI) {
+        next_t -= 2 * Math.PI;
+      }
+      b.curr.jitter.update(next_t);
+
+      const cp = b.curr.position;
+      const np = b.next.position;
+      if (cp.x < np.x) {
+        cp.x = Math.min(np.x, cp.x + update_step);
+      } else if (cp.x > np.x) {
+        cp.x = Math.max(np.x, cp.x - update_step);
+      }
+
+      if (cp.y < np.y) {
+        cp.y = Math.min(np.y, cp.y + update_step);
+      } else if (cp.y > np.y) {
+        cp.y = Math.max(np.y, cp.y - update_step);
+      }
+    }
+  });
 }
 
 function tile_colour(tile) {
@@ -31,48 +159,73 @@ function tile_colour(tile) {
 
 function draw_map() {
   ctx.save();
-  for (var row = 0; row < world.width; ++row) {
-    for (var col = 0; col < world.height; ++col) {
-      ctx.fillStyle = tile_colour(world.map[row + col * world.width]);
-      ctx.fillRect(row * tile_size, col * tile_size, tile_size, tile_size);
+  const s = world.tile_size;
+  for (let row = 0; row < world.width; ++row) {
+    for (let col = 0; col < world.height; ++col) {
+      ctx.fillStyle = tile_colour(world.at(row, col));
+      ctx.fillRect(row * s, col * s, s, s);
     }
   }
   ctx.restore();
 }
 
-function draw_flowers(flowers) {
+function draw_flowers() {
   ctx.save();
-  ctx.fillStyle = 'rgba(255, 255, 0, 0.5)';
-  for (const f of flowers) {
-    const { x, y } = f.position;
-    ctx.fillRect(x * tile_size, y * tile_size, tile_size, tile_size);
+  const s = world.tile_size;
+  for (const { curr, _ } of flowers.values()) {
+    const { x, y } = curr.position;
+    ctx.fillStyle = `rgba(255, 255, 0, ${curr.opacity})`
+    ctx.fillRect(x * s, y * s, s, s);
   }
   ctx.restore();
 }
 
-function draw_bees(bees) {
+function draw_hives() {
   ctx.save();
-  ctx.fillStyle = 'rgba(180, 0, 0, 0.7)';
+  const s = world.tile_size;
+  for (const p of players.data.values()) {
+    const { x, y } = p.position;
+    ctx.fillStyle = `hsla(${p.colour}, 100%, 50%, ${p.opacity})`;
+    ctx.fillRect(x * s, y * s, s, s);
+  }
+  ctx.restore();
+}
+
+function draw_bees() {
+  ctx.save();
   ctx.strokeStyle = 'black';
-  for (const b of bees) {
-    const x = b.position.x * tile_size + tile_size / 2;
-    const y = b.position.y * tile_size + tile_size / 2;
-    const jitter_x = (Math.random() - 0.5) * tile_size * 0.7;
-    const jitter_y = (Math.random() - 0.5) * tile_size * 0.7;
-    const radius = (0.1 + 0.05 * Math.random()) * tile_size;
+  for (const { curr, _ } of bees.values()) {
+    const x = (curr.position.x + 0.5) * world.tile_size;
+    const y = (curr.position.y + 0.5) * world.tile_size;
+    ctx.fillStyle = `hsla(${curr.colour}, 100%, 50%, 0.7)`;
     ctx.beginPath();
-    ctx.arc(x + jitter_x, y + jitter_y, radius, 0, 2 * Math.PI);
+    ctx.arc(x + curr.jitter.x, y + curr.jitter.y, curr.radius, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
   }
   ctx.restore();
 }
 
-function draw(data) {
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   draw_map();
-  draw_flowers(data.flowers);
-  draw_bees(data.bees);
+  draw_flowers();
+  draw_hives();
+  draw_bees();
+}
+
+function main(now) {
+  window.requestAnimationFrame(main);
+  const next_tick = last_tick + tick_length;
+
+  if (now >= next_tick) {
+    const time_since_tick = now - last_tick;
+    const num_ticks = Math.floor(time_since_tick / tick_length);
+    // TODO: refresh all state to 'next' values if num_ticks too large (sleeping?)
+    last_tick += num_ticks * tick_length;
+    animate(num_ticks);
+    draw();
+  }
 }
 
 websocket.onopen = () => write('CONNECTED');
@@ -82,12 +235,15 @@ websocket.onerror = e => write('<span class="error">ERROR:</span> ' + e.data);
 websocket.onmessage = e => {
   const packet = JSON.parse(e.data);
   switch (packet.type) {
-    case 'registration': 
-      world = packet.world; 
-      resize();
+    case 'registration':
+      init(packet.world);
       break;
-    case 'update': 
-      draw(packet.data); 
+    case 'update':
+      update(packet.data);
+      if (last_tick === null) {
+        last_tick = performance.now();
+        main(last_tick);
+      }
       break;
 
     case 'done': write('<span>Received "done"</span>'); break;
